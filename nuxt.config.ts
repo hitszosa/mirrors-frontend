c5f4bdc2de1571bf3a55fdab6babf3c92feccaf0
Takehana <cowpowermax@pm.me>
2023-08-31 09:52:33 +0800

    feat: add dependency `color-mode`

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ export default defineNuxtConfig({
   app: {
     baseURL: 'https://mirror.osa.moe/',
   },
-  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt'],
+  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt', '@nuxtjs/color-mode'],
+  colorMode: {
+    classSuffix: '',
+  },
   devtools: { enabled: true },
 })
