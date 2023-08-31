d0ed8a1772d73996d0b007e3043f8d356781496e
Takehana <cowpowermax@pm.me>
2023-08-31 18:52:39 +0800

    feat: froster glass navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,16 @@
 <template>
-    <div>
-        <Navbar title-name="HITSZ OSA Mirrors">
+    <div class="h-screen">
+        <Navbar title-name="HITSZ OSA Mirrors" class="fixed top-0 left-0 right-0">
             <template v-slot:logo>
                 <Icon size="3em" name="uil:github" />
             </template>
         </Navbar>
-        <main class="flex justify-center p-10">
-            <slot />
-        </main>
-        <div class="py-4"></div>
-        <Footer></Footer>
+        <div class="pt-16">
+            <main class="flex justify-center p-10">
+                <slot />
+            </main>
+            <div class="py-4"></div>
+            <Footer></Footer>
+        </div>
     </div>
 </template>
