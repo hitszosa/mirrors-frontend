c85705f6f4c8a1865675203ba5d95813b5699b64
Takehana <cowpowermax@pm.me>
2023-08-31 14:44:54 +0800

    feat: add nav links

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,15 +1,30 @@
 <template>
-    <header class="flex flex-row items-center px-8 py-3 w-full drop-shadow-sm bg-white dark:bg-black">
+    <header class="flex flex-row items-center px-10 py-3 w-full drop-shadow-sm bg-white dark:bg-black">
         <div class="grow justify-start">
-            <span class="inline-block -translate-y-0.5">
-                <slot name="logo">
-                </slot>
-            </span>
-            <span class="px-1.5"></span>
-            <span class="text-xl font-medium">{{ $props.titleName }}</span>
+            <NuxtLink to="/">
+                <span class="inline-block -translate-y-0.5">
+                    <slot name="logo">
+                    </slot>
+                </span>
+                <span class="px-1.5"></span>
+                <span class="text-xl font-medium">{{ $props.titleName }}</span>
+            </NuxtLink>
         </div>
         <nav class="justify-end">
-            <NavButton link="/">Mirrors</NavButton>
+            <ul class="flex flex-row list-none space-x-8">
+                <li>
+                    <NavButton link="/">Home</NavButton>
+                </li>
+                <li>
+                    <NavButton link="/">Help</NavButton>
+                </li>
+                <li>
+                    <NavButton link="/">News</NavButton>
+                </li>
+                <li>
+                    <NavButton link="/">About</NavButton>
+                </li>
+            </ul>
         </nav>
     </header>
 </template>
