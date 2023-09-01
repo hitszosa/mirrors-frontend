5e7e422a54628691fce41557295c858dd1ee745a
Takehana <cowpowermax@pm.me>
2023-09-01 11:57:46 +0800

    feat: shrink space between

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex flex-row space-x-12 w-full max-w-screen-xl">
-        <section class="flex flex-col space-y-6 grow">
+        <section class="flex flex-col space-y-4 grow">
             <BaseSectionHeading>
                 <template v-slot:icon>
                     <Icon name="icon-park-outline:dropbox"></Icon>
@@ -9,7 +9,7 @@
             </BaseSectionHeading>
             <IndexMirrorList />
         </section>
-        <section class="flex flex-col space-y-6 w-[32em] bg-slate-50 rounded-xl p-7">
+        <section class="flex flex-col space-y-4 w-[32em] bg-slate-50 rounded-xl p-7">
             <BaseSectionHeading>
                 <template v-slot:icon>
                     <Icon name="icon-park-outline:dropbox"></Icon>
