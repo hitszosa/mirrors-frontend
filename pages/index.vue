5133dfe046cbb33f1f881f276ac53f5188e93cc7
Takehana <cowpowermax@pm.me>
2023-09-01 13:14:15 +0800

    feat: improve side column

--- pages/index.vue
+++ pages/index.vue
@@ -9,15 +9,32 @@
             </BaseSectionHeading>
             <IndexMirrorList />
         </section>
-        <section
-            class="flex flex-col space-y-4 w-[32em] bg-slate-50 rounded-xl p-7"
-        >
-            <BaseSectionHeading>
-                <template v-slot:icon>
-                    <Icon name="icon-park-outline:record-disc"></Icon>
-                </template>
-                Get LiveCD
-            </BaseSectionHeading>
+        <section class="flex flex-col space-y-10 w-[28em] bg-slate-50 rounded-xl p-7">
+            <section class="space-y-4">
+                <BaseSectionHeading>
+                    <template v-slot:icon>
+                        <Icon name="icon-park-outline:record-disc"></Icon>
+                    </template>
+                    Get LiveCD and Software
+                </BaseSectionHeading>
+                <p>
+                    在此处下载本站镜像的 Linux 发行版 LiveCD 和开源软件。
+                </p>
+                <UButton color="white" variant="solid" block>
+                    选择 LiveCD 和软件
+                </UButton>
+            </section>
+            <section class="space-y-4">
+                <BaseSectionHeading>
+                    <template v-slot:icon>
+                        <Icon name="icon-park-outline:newspaper-folding"></Icon>
+                    </template>
+                    Recent News
+                </BaseSectionHeading>
+                <p>
+                    本站近期新闻。
+                </p>
+            </section>
         </section>
     </div>
 </template>
