ffcddb811def826c5cbcf6e44745a9c0823844b4
yukkodesu <17264509+yukkodesu@users.noreply.github.com>
2023-09-01 12:21:08 +0800

    feat: add NewsWidget

---
+++ content/news/osa-mock-news.md
@@ -0,0 +1,14 @@
+---
+title: HITSZ OSA 成立啦
+---
+
+# HITSZ OSA 成立啦
+
+HITSZ OSA 成立啦
+HITSZ OSA 成立啦
+HITSZ OSA 成立啦
+
+## 联系我们
+
+-   邮件：
+-   反馈问题：<https://github.com/hitszosa/Mirrors/issues/new>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,9 @@
 <template>
     <div class="flex flex-row space-x-6 w-full max-w-screen-lg">
         <IndexMirrorList class="grow" />
-        <div class="w-96 bg-slate-100">
-            Under construction.
+        <div>
+            <IndexNewsWidget />
+            <IndexNewsWidget />
         </div>
     </div>
 </template>
