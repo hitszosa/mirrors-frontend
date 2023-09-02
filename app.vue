6892035bfe476765bbd822483749c6ea39c921e7
Takehana <cowpowermax@pm.me>
2023-09-02 10:47:46 +0800

    fix: mobile view overflow

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="dark:bg-gray-900">
-    <NuxtLayout>
-      <NuxtPage />
+  <div class="dark:bg-gray-900 min-h-max min-w-max">
+    <NuxtLayout class="bg-inherit">
+      <NuxtPage class="bg-inherit" />
     </NuxtLayout>
   </div>
 </template>
