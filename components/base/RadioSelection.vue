d1ec2c3ba609591270a7aca6ba25f9ab277a65b8
Takehana <cowpowermax@pm.me>
2023-09-02 22:59:15 +0800

    fix: bug introduced in refactor

--- components/base/RadioSelection.vue
+++ components/base/RadioSelection.vue
@@ -1,7 +1,14 @@
 <template>
   <ul class="first-letter:flex flex-col h-min list-none bg-gray-100 dark:bg-gray-800 p-1 rounded-xl whitespace-nowrap">
-    <li v-for="(item, idx) in $props.items" :key="item" @click="onSelect(item, idx)">
-      <button class="transition py-3 px-7 w-full rounded-xl" :class="getButtonStyle(index)">
+    <li
+      v-for="(item, idx) in $props.items"
+      :key="item"
+      @click="onSelect(item, idx)"
+    >
+      <button
+        class="transition py-3 px-7 w-full rounded-xl"
+        :class="getButtonStyle(idx)"
+      >
         {{ item }}
       </button>
     </li>
@@ -17,8 +24,8 @@ const emit = defineEmits<{
     'update:index': [item: string, index: number]
 }>()
 
-const getButtonStyle = (index: number) => {
-  return index === props.index ? 'bg-white dark:bg-gray-900 drop-shadow-sm' : ''
+const getButtonStyle = (idx: number) => {
+  return idx === props.index ? 'bg-white dark:bg-gray-900 drop-shadow-sm' : ''
 }
 const emitUpdate = () => {
   emit('update:index', props.items[0], 0)
