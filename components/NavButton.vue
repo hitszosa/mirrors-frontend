d3be9b7f122b73573d8eb2af6c4dc1ee3a18d003
Takehana <cowpowermax@pm.me>
2023-09-24 18:07:14 +0800

    refactor: clean up styles

--- components/NavButton.vue
+++ components/NavButton.vue
@@ -6,12 +6,11 @@
     <template #default="{ isActive }">
       <a
         :href="$props.link"
-        :class="`transition after:transition-all relative inline-block
+        class="transition after:transition-all relative inline-block
         hocus:text-blue-400 dark:hocus:text-blue-300
-        ${isActive ? 'text-blue-400 dark:text-blue-300' : 'text-slate-500 dark:text-slate-400'}
         after:block after:absolute after:left-1/2 after:-bottom-2 after:-translate-x-1/2 after:rounded-full after:-z-10
-        hocus:after:w-full hocus:after:h-2 hocus:after:bg-slate-100
-        ${isActive ? 'after:w-full after:h-2 after:bg-slate-100' : 'after:bg-blue-400/20 dark:after:bg-blue-300/20 after:w-0 after:h-0'}`"
+        hocus:after:w-full hocus:after:h-2 hocus:after:bg-slate-100"
+        :class="getButtonStyle(isActive)"
       >
         <slot />
       </a>
@@ -23,4 +22,10 @@
 defineProps<{
     link: string
 }>()
+
+const getButtonStyle = (isActive: boolean) => {
+  return isActive
+    ? 'text-blue-400 dark:text-blue-300 after:w-full after:h-2 after:bg-slate-100'
+    : 'text-slate-500 dark:text-slate-400 after:bg-blue-400/20 dark:after:bg-blue-300/20 after:w-0 after:h-0'
+}
 </script>
