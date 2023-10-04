36e63d5d7ca9b36dc6d9931e797056e54d0f54b5
yukkodesu <17264509+yukkodesu@users.noreply.github.com>
2023-10-04 11:15:15 +0800

    feat: add outlink props

--- components/NavButton.vue
+++ components/NavButton.vue
@@ -6,6 +6,7 @@
     <template #default="{ isActive }">
       <a
         :href="$props.link"
+        :target="target"
         class="transition after:transition-all relative inline-block
         hocus:text-blue-400 dark:hocus:text-blue-300
         after:block after:absolute after:left-1/2 after:-bottom-2 after:-translate-x-1/2 after:rounded-full after:-z-10
@@ -19,8 +20,9 @@
 </template>
 
 <script setup lang="ts">
-defineProps<{
+const props = defineProps<{
     link: string
+    isExternalLink?: boolean
 }>()
 
 const getButtonStyle = (isActive: boolean) => {
@@ -28,4 +30,8 @@ const getButtonStyle = (isActive: boolean) => {
     ? 'text-blue-400 dark:text-blue-300 after:w-full after:h-2 after:bg-blue-400/20 dark:after:bg-blue-300/20'
     : 'text-slate-500 dark:text-slate-400 after:bg-blue-400/20 dark:after:bg-blue-300/20 after:w-0 after:h-0'
 }
+
+const target = computed(() => {
+  return props.isExternalLink ? '_blank' : '_self'
+})
 </script>
