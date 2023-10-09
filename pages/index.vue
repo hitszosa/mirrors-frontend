0beecfc6784b9869cf0fac23e70153b57ebca2bb
Takehana <cowpowermax@pm.me>
2023-10-09 13:40:30 +0800

    feat: search

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="flex space-x-12 w-full max-w-screen-xl">
-    <section class="flex flex-col space-y-4 grow">
+    <div class="flex flex-col space-y-4 grow">
       <IndexMirrorList />
-    </section>
-    <section
+    </div>
+    <div
       class="flex flex-col p-7 space-y-10 min-w-[20em] w-[30vw] h-min bg-slate-50 dark:bg-gray-800 rounded-xl"
     >
       <IndexDownloadWidget />
       <IndexNewsWidget />
-    </section>
+    </div>
   </div>
 </template>
 
