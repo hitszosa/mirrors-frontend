f6f2fa44d61bb8a2821a85edd0350de8bf640177
Takehana <cowpowermax@pm.me>
2024-04-16 21:41:53 +0800

    fix: transition bad layout

--- components/base/SlidingTransition.vue
+++ components/base/SlidingTransition.vue
@@ -1,9 +1,9 @@
 <template>
   <transition
-    class="absolute"
-    enter-active-class="transition-all duration-200 ease-out"
+    class="absolute top-0 left-0"
+    enter-active-class="transition-all duration-300 ease-out"
     enter-from-class="opacity-0 translate-y-2"
-    leave-active-class="transition-all duration-200 ease-out"
+    leave-active-class="transition-all duration-300 ease-out"
     leave-to-class="opacity-0 -translate-y-2"
   >
     <slot />
