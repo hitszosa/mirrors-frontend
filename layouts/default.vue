aa4e8f41d43a99a8f7bb790c7d00c7d51bea52d3
Rewired <admin@rewired.moe>
2025-03-31 22:29:34 +0800

    fix: adjust layout padding and navbar positioning in default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <div class="h-full min-h-screen">
+    <div class="h-full min-h-screen pt-14">
       <MiscAprilFools2025 />
     </div>
     <div class="flex flex-col text-slate-800 dark:text-slate-200 w-full">
       <Navbar
         title-name="HITSZ OSA Mirrors"
-        class="top-0 left-0"
+        class="fixed top-0 left-0"
       >
         <template #logo>
           <img
@@ -15,8 +15,7 @@
         </template>
       </Navbar>
       <div
-        class="flex flex-col justify-between min-h-screen bg-inherit
-  pt-20 px-6 md:pt-12 md:px-12"
+        class="flex flex-col justify-between min-h-screen bg-inherit px-6 md:px-12"
       >
         <main class="flex justify-around px-auto py-12">
           <slot />
