2d7d7a8b9c47374608d268f61720bc39c27bf3e4
ZreXoc <zeexoc@outlook.com>
2026-05-04 00:47:52 +0800

    chore: remove nuxt framework from mirrors frontend

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,48 @@
-import { createConfigForNuxt } from '@nuxt/eslint-config/flat'
+import tsParser from '@typescript-eslint/parser'
+import vuePlugin from 'eslint-plugin-vue'
+import vueParser from 'vue-eslint-parser'
 
-export default createConfigForNuxt({
-  features: {
-    stylistic: {
-      semi: false,
-      indent: 2,
-      quotes: 'single',
+const styleRules = {
+  indent: ['error', 2, { SwitchCase: 1 }],
+  quotes: ['error', 'single', { avoidEscape: true }],
+  semi: ['error', 'never'],
+}
+
+export default [
+  {
+    ignores: [
+      '.astro/',
+      'dist/',
+      'node_modules/',
+    ],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      parser: tsParser,
+      sourceType: 'module',
+    },
+    rules: styleRules,
+  },
+  {
+    files: ['**/*.vue'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      parser: vueParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        parser: tsParser,
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      vue: vuePlugin,
+    },
+    rules: {
+      ...styleRules,
+      'vue/html-indent': ['error', 2],
+      'vue/script-indent': ['error', 2, { baseIndent: 0, switchCase: 1 }],
     },
   },
-  ignores: [
-    '.gitignore',
-  ],
-})
+]
