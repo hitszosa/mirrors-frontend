0dddb598b2d5c48cabae4119a7e4f9a880029638
ZreXoc <zeexoc@outlook.com>
2026-05-03 23:13:44 +0800

    refactor: remove nuxt dependencies from shared vue components

---
+++ components/Icon.vue
@@ -0,0 +1,14 @@
+<template>
+  <IconifyIcon
+    :icon="name"
+    v-bind="$attrs"
+  />
+</template>
+
+<script setup lang="ts">
+import { Icon as IconifyIcon } from '@iconify/vue'
+
+defineProps<{
+  name: string
+}>()
+</script>
--- components/NavButton.vue
+++ components/NavButton.vue
@@ -1,26 +1,21 @@
 <template>
-  <NuxtLink
-    :to="$props.link"
-    :custom="true"
+  <a
+    :href="link"
+    :target="isExternalLink ? '_blank' : '_self'"
+    :rel="isExternalLink ? 'noreferrer' : undefined"
+    class="transition-colors after:transition-all relative inline-block
+    hocus:text-blue-400 dark:hocus:text-blue-300
+    after:block after:absolute after:left-1/2 after:-bottom-2 after:-translate-x-1/2 after:rounded-full after:-z-10
+    hocus:after:w-full hocus:after:h-2 hocus:after:bg-slate-100"
+    :class="getButtonStyle(active ?? false)"
   >
-    <template #default="{ isActive }">
-      <a
-        :href="$props.link"
-        :target="target"
-        class="transition-colors after:transition-all relative inline-block
-        hocus:text-blue-400 dark:hocus:text-blue-300
-        after:block after:absolute after:left-1/2 after:-bottom-2 after:-translate-x-1/2 after:rounded-full after:-z-10
-        hocus:after:w-full hocus:after:h-2 hocus:after:bg-slate-100"
-        :class="getButtonStyle(isActive)"
-      >
-        <slot />
-      </a>
-    </template>
-  </NuxtLink>
+    <slot />
+  </a>
 </template>
 
 <script setup lang="ts">
-const props = defineProps<{
+defineProps<{
+  active?: boolean
   link: string
   isExternalLink?: boolean
 }>()
@@ -30,8 +25,4 @@ const getButtonStyle = (isActive: boolean) => {
     ? 'text-blue-400 dark:text-blue-300 after:w-full after:h-2 after:bg-blue-400/20 dark:after:bg-blue-300/20'
     : 'text-slate-500 dark:text-slate-400 after:bg-blue-400/20 dark:after:bg-blue-300/20 after:w-0 after:h-0'
 }
-
-const target = computed(() => {
-  return props.isExternalLink ? '_blank' : '_self'
-})
 </script>
